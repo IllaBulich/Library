29d9d2117a09a2590278584d5ba0810bdd0ed1bf
IllaBulich <mba.2004@mail.ru>
2022-10-14 17:06:15 +0300

    Добавление товаров в корзину

--- library/library/urls.py
+++ library/library/urls.py
@@ -19,4 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('shope.urls')),
+    path('cart/', include(('cart.urls','cart'), namespace='cart')),
 ]
--- library/shope/views.py
+++ library/shope/views.py
@@ -2,6 +2,7 @@ from atexit import register
 from django.shortcuts import redirect, render, get_object_or_404
 from .models import Category, Product,Book
 from .forms import BookForm
+from cart.forms import CartAddProductForm
 
 
 
@@ -47,6 +48,8 @@ def product_detail(request, id, slug):
                                 id=id,
                                 slug=slug,
                                 available=True)
+    cart_product_form = CartAddProductForm()
     return render(request,
                   'store/product/detail.html',
-                  {'product': product})
+                  {'product': product, 
+                   'cart_product_form': cart_product_form})
