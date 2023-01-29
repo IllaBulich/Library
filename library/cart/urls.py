8568fd2fd213e53f3bdaf2f56d2ff5c5af080c33
IllaBulich <mba.2004@mail.ru>
2023-01-29 19:45:31 +0300

    подкоректировоно

--- library/cart/urls.py
+++ library/cart/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.cart_detail, name='cart_detail'),
     path('add/<product_id>/', views.cart_add, name='cart_add'),
     path('remove/<product_id>/', views.cart_remove, name='cart_remove'),
+    path('', views.cart_detail, name='cart_detail'),
 ]
--- library/shope/urls.py
+++ library/shope/urls.py
@@ -3,14 +3,8 @@ from django.urls import path,re_path
 from . import views
 
 urlpatterns = [
-    # re_path(r'^$', views.index , name="main"),
     path('create/', views.create, name="create"),
-    
-    re_path(r'^$', views.product_list, name='product_list'),
-    re_path(r'^(?P<category_slug>[-\w]+)/$',
-        views.product_list,
-        name='product_list_by_category'),
-    re_path(r'^(?P<id>\d+)/(?P<slug>[-\w]+)/$',
-        views.product_detail,
-        name='product_detail'), 
+    path('<category_slug>/', views.product_list, name='product_list_by_category'),
+    path('<id>/<slug>/', views.product_detail, name='product_detail'), 
+    path('', views.product_list, name='product_list'),
 ]
